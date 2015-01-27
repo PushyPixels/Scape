b5ab4d36d99216b9c23897d5a136f270fe07e30d
Maximilian Maroe <drakfyre@gmail.com>
2015-01-26 21:53:59 -0800

    OctoPushy

--- Assets/Scripts/OctoPushyDeath.cs
+++ Assets/Scripts/OctoPushyDeath.cs
@@ -3,8 +3,32 @@ using System.Collections;
 
 public class OctoPushyDeath : Death
 {
+	public GameObject particleEffect;
+	public string animationTrigger = "Death";
+
+	private Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
 	public override void Die()
 	{
+		animator.SetTrigger(animationTrigger);
+		Instantiate(particleEffect,transform.position,transform.rotation);
+
+		StartCoroutine(CheckAnimationDone());
+	}
+
+	IEnumerator CheckAnimationDone()
+	{
+		yield return null;
+		while(animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f && !animator.IsInTransition(0))
+		{
+			yield return null;
+		}
+
 		Destroy(gameObject);
 	}
 }
