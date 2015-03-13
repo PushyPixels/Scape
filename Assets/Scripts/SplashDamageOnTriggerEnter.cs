4a947ce8aaec2c4b697b12ffd091be1b3fa0e7ca
Maximilian Maroe <drakfyre@gmail.com>
2015-03-12 21:27:58 -0700

    Splash damage skill thing

---
+++ Assets/Scripts/SplashDamageOnTriggerEnter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashDamageOnTriggerEnter : MonoBehaviour
+{
+	public GameObject effect;
+	public float radius = 1.0f;
+	public LayerMask layerMask = -1;
+
+	private DamageInfo damageInfo;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		damageInfo = GetComponent<DamageInfo>();
+	}
+
+	void OnTriggerEnter()
+	{
+		//Instantiate our effect
+		Instantiate(effect,transform.position,Quaternion.identity);
+
+		//Do the damage
+		foreach(Collider col in Physics.OverlapSphere(transform.position,radius,layerMask))
+		{
+			col.BroadcastMessage("Damage",damageInfo.damage,SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
