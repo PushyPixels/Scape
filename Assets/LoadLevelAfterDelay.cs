3f762a9b467fdb3a743c637453705dd0a749a46e
Maximilian Maroe <drakfyre@gmail.com>
2015-03-12 20:40:41 -0700

    Level load test stuff

---
+++ Assets/LoadLevelAfterDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelAfterDelay : MonoBehaviour
+{
+	public string levelName;
+	public float delay = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke("LoadLevel",delay);
+	}
+
+	void LoadLevel ()
+	{
+		Application.LoadLevel(levelName);
+	}
+}
