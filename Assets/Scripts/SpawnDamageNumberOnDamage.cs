07ba3590a28cc872486eff823af214b48815d986
Maximilian Maroe <drakfyre@gmail.com>
2015-05-05 21:06:02 -0700

    Big numbers!  GOLD MASTER!

--- Assets/Scripts/SpawnDamageNumberOnDamage.cs
+++ Assets/Scripts/SpawnDamageNumberOnDamage.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 public class SpawnDamageNumberOnDamage : MonoBehaviour
 {
+	public static float runningAverage = 0;
+	public static int numberOfHits = 0;
+	public static int damageHistoryLength = 100;
+
+	public float minSize = 0.025f;
+	public float maxSize = 0.1f;
 	public Text damageTextObject;
 	private GameObject damageCanvas;
 
@@ -15,8 +21,24 @@ public class SpawnDamageNumberOnDamage : MonoBehaviour
 	
 	void Damage(float amount)
 	{
+		//Calculate average damage to determine what's "big" vs "small"
+		runningAverage += amount;
+		numberOfHits++;
+
+		if(numberOfHits > damageHistoryLength)
+		{
+			numberOfHits = damageHistoryLength;
+			runningAverage -= runningAverage/100;
+		}
+		float averageDamage = runningAverage/numberOfHits;
+
+		Debug.Log ("AverageDamage = " + averageDamage.ToString());
+
+
+
 		Text instance = Instantiate(damageTextObject,transform.position,damageTextObject.transform.rotation) as Text;
 		instance.text = ((int)amount).ToString();
 		instance.transform.SetParent(damageCanvas.transform,false);
+		instance.transform.localScale = Vector3.one* Mathf.Lerp(minSize,maxSize,amount/(averageDamage*2));
 	}
 }
